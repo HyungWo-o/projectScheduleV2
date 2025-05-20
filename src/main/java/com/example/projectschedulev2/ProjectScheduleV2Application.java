4032ed6754f795a8643417493f92d3e954487018
HyungWo-o <dbwotns153@naver.com>
2025-05-20 17:35:29 +0900

    init commit

---
+++ src/main/java/com/example/projectschedulev2/ProjectScheduleV2Application.java
@@ -0,0 +1,13 @@
+package com.example.projectschedulev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectScheduleV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectScheduleV2Application.class, args);
+	}
+
+}
